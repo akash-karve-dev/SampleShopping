5728ecea3c371fb38c45133b1df9c4491a08279f
Akash Karve <Akash.Karve@csiweb.com>
2024-03-18 21:06:35 -0500

    added result pattern to shared kernel

---
+++ src/BuildingBlocks/SharedKernel/ResultPattern/Error.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedKernel.ResultPattern
+{
+    public record Error(string Code, string? Description = default)
+    {
+        public static Error None => new("");
+        public static Error NullValue => new("Error.NullValue", "Null value was provided.");
+
+        public static implicit operator Result(Error error) => Result.Failure(error);
+    }
+}
---
+++ src/BuildingBlocks/SharedKernel/ResultPattern/Result.cs
@@ -0,0 +1,29 @@
+﻿namespace SharedKernel.ResultPattern
+{
+    public record Result(bool IsSuccess, Error Error)
+    {
+        public bool IsFailure => !IsSuccess;
+
+        public static Result Success() => new(true, Error.None);
+
+        public static Result<TValue> Success<TValue>(TValue value) => new(value, false, Error.None);
+
+        public static Result Failure(Error error) => new(false, error);
+
+        public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
+    }
+
+    public record Result<TValue> : Result
+    {
+        private TValue? _value { get; }
+
+        public Result(TValue? value, bool isSuccess, Error error) : base(isSuccess, error)
+        {
+            _value = value;
+        }
+
+        public TValue? Value => IsSuccess ? _value : throw new InvalidOperationException("Value for failure result cannot be accessed");
+
+        public static implicit operator Result<TValue>(TValue value) => value != null ? value : Failure<TValue>(Error.NullValue);
+    }
+}
